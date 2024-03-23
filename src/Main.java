587c2abda706ea25f1a1a02709979c89d2489fcd
User <essena23@gmail.com>
2024-03-23 00:02:55 -0400

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+    public static void checkLeapYear(int year) {
+        if (year > 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("Год високосный");
+        } else {
+            System.out.println("Год не високосный");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task 1
+        System.out.println("task 1");
+        checkLeapYear(3568);
+        checkLeapYear(1574);
+        checkLeapYear(2000);
+        checkLeapYear(20100);
+        //task 2
+        System.out.println("task 2");
     }
 }
